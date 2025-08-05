07bf2846059f103611dc0ab52e2e151b11693fae
Aiden Ghim <aiden.bj.ghim@gmail.com>
2025-08-05 13:35:47 +0900

    New template layout
    New feature coming: enums for tokens & nonterminals

--- template.rs
+++ template.rs
@@ -13,11 +13,17 @@ struct Production(u32, Vector<u32>);
  * Production(2, vec![2, 1])
  */
 
+
+/*
+ * terminals: 0, 1, 2, 3, 4, ...
+ * nonterminals: 256, 257, ...
+ */
+
 const rules: [Production; NUMBER_OF_RULES] = [
     /* DEFINE_YOUR_RULES */
 ];
 
-const actiontab: [[u32; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
+const actiontab: [[Action; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
     /* state 0 */ [/* sym 0 */ Action::Shift(3), /* sym 1 */ Action::Shift(2), ...],
     /* state 1 */ [...],
     ...
@@ -26,6 +32,16 @@ const actiontab: [[u32; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
 const gototab: [[u32; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
     /* define it here */
 ];
+
+const nonterm_factor = 256;
+
+fn getaction(state: u32, symbol: u32) -> Action {
+	actiontab[state][symbol]
+}
+
+fn getgoto(state: u32, symbol: u32) -> u32 {
+	gototab[state][symbol-nonterm_factor]
+}
 
 /*
  * reference actiontab:
