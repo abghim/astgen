9a8ea990eb00d10550e62150d72ca0ec7c0bd669
Aiden Ghim <aiden.bj.ghim@gmail.com>
2025-08-05 11:12:53 +0900

    Update template.rs

--- template.rs
+++ template.rs
@@ -7,30 +7,46 @@ enum Action {
     Error
 }
 
-/* debug: String for human-readable token output */
-let SymbolMap: HashMap<u32, &str>;
-
-SymbolMap.put(0, "EOI");
-SymbolMap.put(1, "E");
-...
-
-struct Production(Symbol, Vector<u32>);
+struct Production(u32, Vector<u32>);
 
 /*
- * Production(2, vec![1])
+ * Production(2, vec![2, 1])
  */
 
 const rules: [Production; NUMBER_OF_RULES] = [
-    DEFINE_YOUR_RULES
+    /* DEFINE_YOUR_RULES */
 ];
 
-const actiontab: [[HashMap<Symbol, Action>]; NUMBER_OF_STATES] = [
-    /* symbol 1 */ [/* state 0 */ Action::Shift(3), /* state 1 */ Action::Shift(2), ...],
-    /* symbol 2 */ [...],
+const actiontab: [[u32; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
+    /* state 0 */ [/* sym 0 */ Action::Shift(3), /* sym 1 */ Action::Shift(2), ...],
+    /* state 1 */ [...],
     ...
-]
+];
+
+const gototab: [[u32; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
+    /* define it here */
+];
 
 /*
  * reference actiontab:
- * actiontab[symbol, state]
+ * actiontab[state][symbol]
+ */
+
+fn main() {
+    /* debug: String for human-readable token output */
+    let SymbolMap: HashMap<u32, &str>;
+
+    SymbolMap.put(0, "EOI");
+    SymbolMap.put(1, "E");
+    /* ... */
+}
+
+/*
+ * To sum it up:
+ * - boilerplate (defs)
+ * - rules list
+ * - actiontab
+ * - gototab
+ * - main + symbolmap (debug)
+ * - PARSING LOGIC
  */
