696f7dff94198b543a5b81f43827774aa139f3f2
Aiden Ghim <aiden.bj.ghim@gmail.com>
2025-08-07 22:26:48 +0900

    Change template

--- template.rs
+++ template.rs
@@ -5,7 +5,12 @@ enum Action {
     Reduce(u32),
     Accept,
     Error
-}
+};
+
+enum GotoState {
+	State(u32),
+	Error
+};
 
 struct Production(u32, Vector<u32>);
 
@@ -17,6 +22,29 @@ struct Production(u32, Vector<u32>);
 /*
  * terminals: 0, 1, 2, 3, 4, ...
  * nonterminals: 256, 257, ...
+ */
+
+#[repr(u32)]
+enum Terminal {
+	Multiplication = 0,
+	Addition = 1,
+	Identifier = 2,
+	EndOfInput = 3,
+	...,
+	COUNT
+};
+
+#[repr(u32)]
+enum Nonterminal {
+	Expr = 0,
+	Term = 1,
+	Start = 2,
+	...,
+	COUNT
+};
+
+/*
+ * Nonterminals as u32
  */
 
 const rules: [Production; NUMBER_OF_RULES] = [
@@ -32,16 +60,6 @@ const actiontab: [[Action; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
 const gototab: [[u32; NUMBER_OF_SYMBOLS]; NUMBER_OF_STATES] = [
     /* define it here */
 ];
-
-const nonterm_factor = 256;
-
-fn getaction(state: u32, symbol: u32) -> Action {
-	actiontab[state][symbol]
-}
-
-fn getgoto(state: u32, symbol: u32) -> u32 {
-	gototab[state][symbol-nonterm_factor]
-}
 
 /*
  * reference actiontab:
